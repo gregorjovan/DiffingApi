5a2a6fcd8ac9fb90de491fa7155c18f8884ba62d
gregorjovan <gregorjovan@gmail.com>
2026-04-14 17:13:02 +0200

    feat: add OpenAPI and Swagger UI support

--- DiffingApi/Program.cs
+++ DiffingApi/Program.cs
@@ -5,7 +5,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<DiffContentStore>();
 
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/openapi/v1.json", "Diffing API");
+    });
+}
+
+app.MapGet("/", () => Results.Redirect("/swagger")).ExcludeFromDescription();
 app.MapApplicationEndpoints();
 
 app.Run();
