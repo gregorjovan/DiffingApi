d8d3f1c562c7a093ff00a93353c1c0366e6b8e39
gregorjovan <gregorjovan@gmail.com>
2026-04-14 15:55:11 +0200

    refactor: move endpoint mappings to Endpoints folder

---
+++ DiffingApi/Endpoints/ApplicationEndpoints.cs
@@ -0,0 +1,41 @@
+﻿using DiffingApi.Contracts;
+using DiffingApi.Services;
+
+namespace DiffingApi.Endpoints;
+
+public static class ApplicationEndpoints
+{
+    public static void MapApplicationEndpoints(this WebApplication app)
+    {
+        app.MapPut("/v1/diff/{id}/left", (string id, DiffRequest request, DiffContentStore store) =>
+        {
+            store.SetLeft(id, request.Data);
+            return Results.Created($"/v1/diff/{id}/left", null);
+        });
+
+        app.MapPut("/v1/diff/{id}/right", (string id, DiffRequest request, DiffContentStore store) =>
+        {
+            store.SetRight(id, request.Data);
+            return Results.Created($"/v1/diff/{id}/right", null);
+        });
+
+        app.MapGet("/v1/diff/{id}", (string id, DiffContentStore store) =>
+        {
+            if (!store.TryGet(id, out var entry) || entry?.Left is null || entry.Right is null)
+                return Results.NotFound();
+
+            var leftBytes = Convert.FromBase64String(entry.Left);
+            var rightBytes = Convert.FromBase64String(entry.Right);
+
+            if (leftBytes.SequenceEqual(rightBytes))
+                return Results.Ok(new { diffResultType = "Equals" });
+
+            if (leftBytes.Length != rightBytes.Length)
+                return Results.Ok(new { diffResultType = "SizeDoNotMatch" });
+
+            var diffs = DiffCalculator.FindDiffs(leftBytes, rightBytes);
+
+            return Results.Ok(new { diffResultType = "ContentDoNotMatch", diffs });
+        });
+    }
+}
--- DiffingApi/Program.cs
+++ DiffingApi/Program.cs
@@ -1,5 +1,4 @@
-using System.Linq;
-using DiffingApi.Contracts;
+using DiffingApi.Endpoints;
 using DiffingApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,50 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<DiffContentStore>();
 
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
-app.MapPut("/v1/diff/{id}/left", (string id, DiffRequest request, DiffContentStore store) =>
-{
-    store.SetLeft(id, request.Data);
-
-    return Results.Created($"/v1/diff/{id}/left", null);
-});
-
-app.MapPut("/v1/diff/{id}/right", (string id, DiffRequest request, DiffContentStore store) =>
-{
-    store.SetRight(id, request.Data);
-
-    return Results.Created($"/v1/diff/{id}/right", null);
-});
-
-app.MapGet("/v1/diff/{id}", (string id, DiffContentStore store) =>
-{
-    if (!store.TryGet(id, out var entry) || entry is null || entry.Left is null || entry.Right is null)
-    {
-        return Results.NotFound();
-    }
-
-    var leftData = entry.Left;
-    var rightData = entry.Right;
-
-    var leftBytes = Convert.FromBase64String(leftData);
-    var rightBytes = Convert.FromBase64String(rightData);
-
-    if (leftBytes.SequenceEqual(rightBytes))
-    {
-        return Results.Ok(new { diffResultType = "Equals" });
-    }
-
-    if (leftBytes.Length != rightBytes.Length)
-    {
-        return Results.Ok(new { diffResultType = "SizeDoNotMatch" });
-    }
-
-    var diffs = DiffCalculator.FindDiffs(leftBytes, rightBytes);
-
-    return Results.Ok(new { diffResultType = "ContentDoNotMatch", diffs });
-});
+app.MapApplicationEndpoints();
 
 app.Run();
 
